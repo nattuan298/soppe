c610bd02d566e2e3a4a6f7952dea566bb3101a63
PhamDucMinh0206 <70513865+PhamDucMinh0206@users.noreply.github.com>
2023-04-02 15:15:39 +0700

    add address (#7)
    * address
    
    * address
    
    * s
    
    * address
    
    * add, update, remove address

---
+++ BE/src/modules/address/address.constant.ts
@@ -0,0 +1,3 @@
+export enum AddressResponseMessage {
+  NotFound = 'Address is not found.',
+}
---
+++ BE/src/modules/address/address.service.ts
@@ -0,0 +1,66 @@
+import { InjectModel } from '@nestjs/mongoose';
+import { ADDRESS_MODEL } from './address.schema';
+import { Model } from 'mongoose';
+import { IAddressDocument } from './address.interface';
+import { CreateAddressDto } from './dto/create-adrress.dto';
+
+import { NotFoundException } from '@nestjs/common';
+import { UpdateAddressDto } from './dto/update-address.dto';
+import { AddressResponseMessage } from './address.constant';
+
+export class AddressService {
+  constructor(
+    @InjectModel(ADDRESS_MODEL)
+    private readonly addressModel: Model<IAddressDocument>,
+  ) {}
+
+  async create(createAddress: CreateAddressDto, userId: string) {
+    createAddress.firstName = createAddress.firstName.trim();
+    createAddress.lastName = createAddress.lastName.trim();
+    createAddress.address = createAddress.address.trim();
+    return await this.addressModel.create({
+      ...createAddress,
+      userId,
+    });
+  }
+
+  async findAll(userId: string) {
+    const address = await this.addressModel.find({ userId }).lean();
+    if (!address.length) {
+      return {
+        data: [],
+      };
+    }
+    return {
+      data: address,
+    };
+  }
+  async findOne(id: string) {
+    const address = await this.addressModel.findById(id);
+    if (!address) {
+      return;
+    }
+    return address;
+  }
+  async update(id: string, updateAddressDto: UpdateAddressDto) {
+    updateAddressDto.address = updateAddressDto.address.trim();
+    updateAddressDto.firstName = updateAddressDto.firstName.trim();
+    updateAddressDto.lastName = updateAddressDto.lastName.trim();
+    const existingAddressLoop = await this.addressModel.findByIdAndUpdate(
+      { _id: id },
+      updateAddressDto,
+      { new: true },
+    );
+    if (!existingAddressLoop) {
+      throw new NotFoundException(AddressResponseMessage.NotFound);
+    }
+  }
+  async remove(id: string) {
+    const addressDelete = await this.addressModel.findByIdAndDelete({
+      _id: id,
+    });
+    if (!addressDelete) {
+      throw new NotFoundException(AddressResponseMessage.NotFound);
+    }
+  }
+}
